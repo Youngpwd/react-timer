99bb1f6eacd0433b57202e7f4ee60cf709e0cba6
Charles Young <cbyoung415@gmail.com>
2022-07-28 15:55:22 -0700

    trying to fix stutter

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -5,8 +5,8 @@ export function Timer({ time }) {
   return (
     <div className="timer">
       <h2>
-        <span>{("0" + Math.floor((time / 3600000))).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 3600000))).slice(-2)} : </span>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </h2>
